b03628cce131bc3c5754db1158013e1abd8b0479
jandird <jandird@mcmaster.ca>
2019-01-23 19:58:49 -0500

    Made raw homepage

--- STopify/views.py
+++ STopify/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def stopify(request):
-    return HttpResponse("STopify")
+    # return HttpResponse("STopify")
+    return render(request, 'stopify.html')
